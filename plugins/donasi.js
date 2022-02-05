6c11385db866b47566fbf510f40dec76d6eaa1e0
BLXB567 <97624291+BLXB567@users.noreply.github.com>
2022-02-05 12:02:23 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,10 +1,11 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat [085713964963]
-│ • Gopay [085713964963]
+│ • Exis [083117436733]
+│ • Gopay [6283117436733]
 ╰────
 ╭─「 Hubungi 」
-│ > Ingin donasi? Wa.me/6281390658325
+│ > Ingin donasi? Wa.me/6283117436733
+│ > ingin sewa? hubungi owner Wa.me/6283117436733
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
