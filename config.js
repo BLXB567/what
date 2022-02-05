aa42aa07d9fc18adc430d847df58f83d3e825fe8
BLXB567 <97624291+BLXB567@users.noreply.github.com>
2022-02-05 09:57:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ global.DeveloperMode = 'false' //true Or false
 global.linkGC = ['https://chat.whatsapp.com/Ie3xBHdL22A560EBWRwFwI', 'https://chat.whatsapp.com/GsZcFsjIuVOGlJYIgrTJzg']
 global.Owner = {
   // use the phone number with the country code, for example Indonesia '62' America '1' etc
-  '62857139649630': { // put your number here
-    name: 'Owner Metro Bot/Creator games-wabot',
+  '6283117436733': { // put your number here
+    name: 'BLZXB5',
     isDev: false, // if true this number will send if bot have bug and developer mode true
     isReport: false, // if true this number will send if user use command /report or /join
     isCreator: true // if true this number will send if user use command /owner or /creator
